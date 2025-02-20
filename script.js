ff4712d3984272f179b1e674a67d37b048734c86
abla-code <abla24rh@student.ju.se>
2025-02-20 11:22:02 +0100

    js file added, interactivity

---
+++ script.js
@@ -0,0 +1,9 @@
+
+/* COLOR CHANGES FOR MY ID*/
+document.getElementById('logo').addEventListener('mouseover', function () {
+    this.style.color = '#964B00'; 
+  });
+  
+  document.getElementById('logo').addEventListener('mouseout', function () {
+    this.style.color = ''; 
+  });
